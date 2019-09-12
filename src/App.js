f47ca5a5133dd9c1eb00db7878539306082baa07
Danis Zaidan <daniszaidan@gmail.com>
2019-09-12 09:34:21 +0700

    Create Layout and Card Todo List

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 import './zero.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="col col-6 col-m-12 half-left">
+        a
+      </div>
+      <div className="col col-6 col-m-12 half-right">
+
+        <div className="card">
+          <div class='col-1 col-s-12 d-flex flex-center'>
+            s
+          </div>
+          <div class='col-10 col-s-12 list-frame'>
+            <div className="title-frame">
+              <p>mengerjakan pr mengerjakan pr mengerjakan pr</p>
+            </div>
+            <div className="tag-frame">
+              <div className="tag">school</div>
+              <div className="tag">school</div>
+              <div className="tag">school</div>
+            </div>
+          </div>
+          <div class='col-1 col-s-12 d-flex flex-center'>
+            s
+          </div>
+        </div>
+
+      </div>
+    </main>
   );
 }
 
