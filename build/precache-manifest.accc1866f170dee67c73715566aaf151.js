05ae88e28bb25aed80167dd8c34ac65aa5001a3e
Danis Zaidan <daniszaidan@gmail.com>
2019-09-25 10:40:18 +0700

    build project

---
+++ build/precache-manifest.accc1866f170dee67c73715566aaf151.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "614cbaa0c236cd7148b97b0b8b3f4b9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd9c74af84353673f3b",
+    "url": "/static/css/main.e36a331d.chunk.css"
+  },
+  {
+    "revision": "f6a129fb032cc8f89b0b",
+    "url": "/static/js/2.8925e3d0.chunk.js"
+  },
+  {
+    "revision": "ddd9c74af84353673f3b",
+    "url": "/static/js/main.9601be47.chunk.js"
+  },
+  {
+    "revision": "cb4fd463e7184abd8f6a",
+    "url": "/static/js/runtime~main.d52357dd.js"
+  },
+  {
+    "revision": "f5e4f17c1f6e93d299fd91d8b7dcc9bd",
+    "url": "/static/media/background.f5e4f17c.jpeg"
+  },
+  {
+    "revision": "de7e31a8c2329dfcd0f176b9ef03e001",
+    "url": "/static/media/logo.de7e31a8.png"
+  }
+]);
